3a255267982f36f079647e9ea81888ce27b26e42
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-01-07 10:52:44 +0300

    game table added

--- src/app/game-page/game-page.component.ts
+++ src/app/game-page/game-page.component.ts
@@ -1,15 +1,29 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game-page',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgFor, NgIf],
   templateUrl: './game-page.component.html',
   styleUrl: './game-page.component.css'
 })
 export class GamePageComponent implements OnInit{
   gameId: string | null = null;
+  rows = Array(8).fill(0); // 8 satır
+  cols = Array(8).fill(0); // 8 sütun
+  pieces: string[][] = [
+    ['rook-black', 'knight-black', 'bishop-black', 'queen-black', 'king-black', 'bishop-black', 'knight-black', 'rook-black'],
+    ['pawn-black', 'pawn-black', 'pawn-black', 'pawn-black', 'pawn-black', 'pawn-black', 'pawn-black', 'pawn-black'],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['pawn-white', 'pawn-white', 'pawn-white', 'pawn-white', 'pawn-white', 'pawn-white', 'pawn-white', 'pawn-white'],
+    ['rook-white', 'knight-white', 'bishop-white', 'queen-white', 'king-white', 'bishop-white', 'knight-white', 'rook-white'],
+  ];
+  selectedCell: { row: number; col: number } | null = null;
 
   constructor(private route: ActivatedRoute) {}
   
@@ -17,5 +31,29 @@ export class GamePageComponent implements OnInit{
     // URL'deki GameId parametresini al
     this.gameId = this.route.snapshot.paramMap.get('GameId');
     console.log('GameId:', this.gameId);
+
+    // get the game info
+
+    if (3 == 3){
+      this.initializeBlackBoard();
+    }
+  }
+
+  onCellClick(row: number, col: number): void {
+    console.log(`Clicked cell: Row ${row}, Col ${col}`);
+    this.selectedCell = { row, col };
+  }
+
+  initializeBlackBoard(){
+    this.pieces = [
+      ['rook-white', 'knight-white', 'bishop-white', 'king-white', 'queen-white', 'bishop-white', 'knight-white', 'rook-white'],
+      ['pawn-white', 'pawn-white', 'pawn-white', 'pawn-white', 'pawn-white', 'pawn-white', 'pawn-white', 'pawn-white'],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['', '', '', '', '', '', '', ''],
+      ['pawn-black', 'pawn-black', 'pawn-black', 'pawn-black', 'pawn-black', 'pawn-black', 'pawn-black', 'pawn-black'],
+      ['rook-black', 'knight-black', 'bishop-black', 'king-black', 'queen-black', 'bishop-black', 'knight-black', 'rook-black'],
+    ];
   }
 }
