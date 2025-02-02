9345d13da43c6399182486be6e38fcfe3825be44
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-02-02 14:21:31 +0300

    university-players-page design improved

--- src/app/university-players-page/university-players-page.component.ts
+++ src/app/university-players-page/university-players-page.component.ts
@@ -1,14 +1,37 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-university-players-page',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, RouterLink, TranslateModule, NgIf],
   templateUrl: './university-players-page.component.html',
   styleUrl: './university-players-page.component.css'
 })
-export class UniversityPlayersPageComponent {
+export class UniversityPlayersPageComponent implements OnInit, OnDestroy {
   universityName: string = "Yıldız Teknik Üniversitesi";
   universityPlayers: Array<{userName: string, totalPoint: number}> = [{userName: "AMD", totalPoint: 3}];
+  isMobile: boolean = false;
+  isUniPlayer: boolean = true;
+  isUniAdmin: boolean = true;
+  ngOnInit(): void {
+    window.addEventListener('resize', () => {
+      this.checkIfMobile();
+    });
+  }
+  ngOnDestroy() {
+    window.removeEventListener('resize', () => {
+      this.checkIfMobile();
+    });
+  }
+
+  onJoinRequest(){
+
+  }
+
+  checkIfMobile() {
+    this.isMobile = window.innerWidth <= 768;
+  }
 }
