60fcb1b7e2e1a6adada69372bfdf917c8f861c3e
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-02-09 21:32:46 +0300

    agreement checkboxes added to register-page

--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
 import { User } from '../models/ui-models/user-model';
@@ -15,35 +15,132 @@ import { passwordMatchValidator } from '../validators/password-match-validator';
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
-export class RegisterPageComponent implements OnInit, OnDestroy {
+export class RegisterPageComponent implements OnInit, OnDestroy, AfterViewInit {
   isMobile: boolean = false;
   registerForm: FormGroup; 
-  
-  constructor(private fb: FormBuilder,  private translate: TranslateService) {
+  isUserTermsModalOpen = false;
+  isFairPlayTermsModalOpen = false;
+  hasScrolledToBottomUser = false;
+  hasScrolledToBottomFairPlay = false;
+  isUserAgreementRead = false;
+  isFairPlayAgreementRead = false;
+  agreementType = "";
+  private scrollListener: (() => void) | null = null;
+  @ViewChild('userTermsContainer') userTermsContainer!: ElementRef;
+  @ViewChild('fairPlayTermsContainer') fairPlayTermsContainer!: ElementRef;
+
+  constructor(private fb: FormBuilder,  private translate: TranslateService, private renderer: Renderer2) {
     this.registerForm = this.fb.group({
       userName: ['', [Validators.required], [userNameAsyncValidator]],
       email: ['', [Validators.required, emailValidator()], [emailPatternAsyncValidator]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', [Validators.required, passwordMatchValidator()]],
       gender: ['', [Validators.required]],
+      userTerms: [false, [Validators.requiredTrue]],
+      fairPlay: [false, [Validators.requiredTrue]]
     });   
   }
+  
   ngOnInit(): void {
     window.addEventListener('resize', () => {
       this.checkIfMobile();
     });
+    this.registerForm.get('userTerms')?.disable();
+    this.registerForm.get('fairPlay')?.disable();
   }
+
   ngOnDestroy() {
     window.removeEventListener('resize', () => {
       this.checkIfMobile();
     });
   }
+
+  ngAfterViewInit() {
+    setTimeout(() => this.checkScroll(), 100);
+  }
+
   checkIfMobile() {
     this.isMobile = window.innerWidth <= 768;
   }
+
   onRegister(){
+    this.registerForm.markAllAsTouched();
+    const isFairPlayDisabled = this.registerForm.get('fairPlay')?.disabled;
+    const isUserAgreementDisabled = this.registerForm.get('userTerms')?.disabled;
+    !this.isFairPlayAgreementRead && this.registerForm.get('fairPlay')?.enable();
+    !this.isUserAgreementRead && this.registerForm.get('userTerms')?.enable();
+
     if (this.registerForm.valid) {
       console.log(this.registerForm.value);
+    }
+    else{
+      isFairPlayDisabled && this.registerForm.get('fairPlay')?.disable();
+      isUserAgreementDisabled && this.registerForm.get('userTerms')?.disable();
+    }
+  }
+
+  openTermsModal(event: Event, type: string) {
+    this.agreementType = type;
+    event.preventDefault();
+    if(this.agreementType === 'user'){
+      this.isUserTermsModalOpen = true;
+    }
+    else if(this.agreementType === 'fair-play'){
+      this.isFairPlayTermsModalOpen = true;
+    }
+
+    setTimeout(() => {
+      this.checkScroll();
+      this.addScrollListener();
+    }, 100);
+  }
+
+  closeTermsModal() {
+    if(this.agreementType === 'user'){
+      this.isUserTermsModalOpen = false;
+      this.registerForm.get('userTerms')?.enable();
+    }
+    else if(this.agreementType === 'fair-play'){
+      this.isFairPlayTermsModalOpen = false;
+      this.registerForm.get('fairPlay')?.enable();
+    }
+    this.removeScrollListener();
+  }
+
+  checkScroll() {
+    if(this.agreementType === 'user'){
+      if (!this.userTermsContainer) return;
+      const div = this.userTermsContainer.nativeElement;
+      const scrollBottom = div.scrollTop + div.clientHeight;
+      const tolerance = 10;
+      this.hasScrolledToBottomUser = scrollBottom >= div.scrollHeight - tolerance;
+    }
+    else if(this.agreementType === 'fair-play'){
+      if (!this.fairPlayTermsContainer) return;
+      const div = this.fairPlayTermsContainer.nativeElement;
+      const scrollBottom = div.scrollTop + div.clientHeight;
+      const tolerance = 10;
+      this.hasScrolledToBottomFairPlay = scrollBottom >= div.scrollHeight - tolerance;
+    }
+  }
+
+  addScrollListener() {
+    if(this.agreementType === 'user'){
+      if (this.userTermsContainer) {
+        this.renderer.listen(this.userTermsContainer.nativeElement, 'scroll', () => this.checkScroll());
+      }
+    }
+    else if(this.agreementType === 'fair-play'){
+      if (this.fairPlayTermsContainer) {
+        this.renderer.listen(this.fairPlayTermsContainer.nativeElement, 'scroll', () => this.checkScroll());
+      }
+    }
+  }
+
+  removeScrollListener() {
+    if (this.scrollListener) {
+      this.scrollListener();
+      this.scrollListener = null;
     }
   }
 }
