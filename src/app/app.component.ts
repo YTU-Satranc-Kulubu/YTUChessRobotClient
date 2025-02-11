0e84170260c82fcd2d39860f2c498e23b68f087e
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-02-11 14:37:44 +0300

    footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslateModule, NavbarComponent, NavbarComponent],
+  imports: [RouterOutlet, TranslateModule, NavbarComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
