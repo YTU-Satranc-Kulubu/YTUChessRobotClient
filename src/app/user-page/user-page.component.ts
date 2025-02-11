7a1880728ab27dc8fc996729b65249e2d3a37fdb
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-02-11 14:21:25 +0300

    user-page design and form validation improved

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { User } from '../models/ui-models/user-model';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [TranslateModule, FormsModule],
+  imports: [TranslateModule, ReactiveFormsModule, NgIf],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.css'
 })
 export class UserPageComponent {
   isMobile: boolean = false;
+  updateForm: FormGroup; 
   user: User = {
     id: 0,
     name: '',
@@ -22,10 +24,23 @@ export class UserPageComponent {
     isDeleted: false,
   };
   gender: string = "";
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.updateForm = this.fb.group({
+      userName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      gender: ['', [Validators.required]]
+    });   
+  }
   ngOnInit(): void {
     window.addEventListener('resize', () => {
       this.checkIfMobile();
+    });
+
+    this.updateForm.patchValue({
+      userName: "AMD",
+      email: "mahir.demirelli@std.yildiz.edu.tr",
+      gender: "male"
     });
   }
   ngOnDestroy() {
@@ -37,7 +52,10 @@ export class UserPageComponent {
     this.isMobile = window.innerWidth <= 768;
   }
   onUpdate(){
-
+    this.updateForm.markAllAsTouched();
+    if (this.updateForm.valid) {
+      console.log(this.updateForm.value);
+    }
   }
   onDelete(){
     
