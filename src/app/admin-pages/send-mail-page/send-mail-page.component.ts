dceb8729e1d8787f3fc6231bc8b31bf7e16fa80d
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2025-02-12 20:42:38 +0300

    send-mail-page added

--- src/app/admin-pages/send-mail-page/send-mail-page.component.ts
+++ src/app/admin-pages/send-mail-page/send-mail-page.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgFor, NgIf } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-send-mail-page',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, NgFor, NgIf, ReactiveFormsModule],
   templateUrl: './send-mail-page.component.html',
   styleUrl: './send-mail-page.component.css'
 })
 export class SendMailPageComponent {
+  mailForm: FormGroup;
+  userNames: string[] = ['AMD-1', 'AMD-2'];
 
+  constructor(private fb: FormBuilder) {
+    this.mailForm = this.fb.group({
+      receiver: ['', [Validators.required]],
+      message: ['', [Validators.required]]
+    });   
+  }
+
+  onSubmit(){
+    
+  }
 }
